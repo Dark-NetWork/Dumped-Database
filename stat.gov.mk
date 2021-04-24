c006107dcaecc250184f3abeadbf221f7d4893c9
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-24 10:50:26 +0300

    Update stat.gov.mk

--- stat.gov.mk
+++ stat.gov.mk
@@ -45,3 +45,208 @@ database management system users password hashes:
     password hash: NULL
 [*] sa [1]:
     password hash: NULL
+
+Database: WEB_APLIKACII
+[19 tables]
++--------------------------------+
+| CODED_A                        |
+| DOWNLOADS                      |
+| Iminja18                       |
+| Iminja_2016                    |
+| Iminja_2016                    |
+| Iminja_2018                    |
+| Iminja_2019                    |
+| Iminja_stara2017               |
+| Iminja_stara2017               |
+| ONP_NASMESTA                   |
+| ONP_OPSTINI                    |
+| ONP_PODATOCI                   |
+| Preziminja18                   |
+| Preziminja_2016                |
+| Preziminja_2016                |
+| Preziminja_2018                |
+| Preziminja_2019                |
+| Preziminja_stara2017           |
+| Preziminja_stara2017           |
++--------------------------------+
+
+Database: master
+[5 tables]
++--------------------------------+
+| spt_fallback_db                |
+| spt_fallback_dev               |
+| spt_fallback_usg               |
+| spt_monitor                    |
+| spt_values                     |
++--------------------------------+
+
+Database: WEB_NOVA
+[92 tables]
++--------------------------------+
+| AKTIVNOSTI_AL                  |
+| AKTIVNOSTI_AL                  |
+| ARHIVA_PUBL                    |
+| ARHIVA_PUBL                    |
+| CODED_A                        |
+| DOWNLOADS                      |
+| DOWNLOADSXLS                   |
+| EDICIIPUBLIKACII               |
+| FORMATPUBLIKUVANJE             |
+| GLAVNIOBLASTI_AL               |
+| GLAVNIOBLASTI_AL               |
+| GODINI                         |
+| GrafikInterActive              |
+| GrafikInterActive              |
+| INDIKATORIGLAVNA_AL            |
+| INDIKATORIGLAVNA_AL            |
+| INDIKATORI_AL                  |
+| INDIKATORI_AL                  |
+| INSTITUCII                     |
+| IZVESTAJKVALITETGLAVNA         |
+| IZVESTAJKVALITETGLAVNA         |
+| Indikatori_TSal                |
+| Indikatori_TSal                |
+| Infografik_Backup              |
+| Infografik_Backup              |
+| InterActivGrafik               |
+| JAZIK                          |
+| KALENDAR_AL_2                  |
+| KALENDAR_AL_2                  |
+| KALENDAR_AL_2                  |
+| KALENDAR_INFOGRAFIK_al         |
+| KALENDAR_INFOGRAFIK_al         |
+| KALENDAR_MESECI                |
+| KALENDAR_NOV7                  |
+| KALENDAR_NOV_VNES              |
+| KALENDAR_V2                    |
+| KALKULATOR_COICOP              |
+| KLASIFIKACIIGLAVNA             |
+| KLASIFIKACII_ON_EU             |
+| KLASIFIKACII_ON_EU             |
+| KORESPONDENTNI_TABELI          |
+| LEGISLATIVA                    |
+| MAPI_PY                        |
+| META_NOVA                      |
+| META_NOVA                      |
+| METODOLOGII                    |
+| METODOLOSKIOBJASNUVANJA_ARHIVA |
+| METODOLOSKIOBJASNUVANJA_ARHIVA |
+| NACIONALNI_GLAVNA              |
+| NACIONALNI_KLASIFIKACII        |
+| NOV_KALENDAR                   |
+| OBLASTEKSTRA                   |
+| OBLASTI_AL                     |
+| OBLASTI_AL                     |
+| OBLASTI_MAKSTAT                |
+| OBLASTOPSTO                    |
+| ONP_NASMESTA                   |
+| ONP_OPSTINI                    |
+| ONP_PODATOCI                   |
+| OblastInActive                 |
+| OblastiInterActive             |
+| POPISNI_REONI                  |
+| POPIS_MEDIUMI_EN               |
+| POPIS_MEDIUMI_MK               |
+| PRIBIRANJEPODATOCI             |
+| PUBLIKACIIGLAVNA_              |
+| PUBLIKACIIGLAVNA_              |
+| PUBLIKACII_AL                  |
+| PUBLIKACII_AL                  |
+| RSS                            |
+| Revizija                       |
+| SEDUMDESET                     |
+| SOOPSTENIJATEKST_AL            |
+| SOOPSTENIJATEKST_AL            |
+| SOOPSTENIJA_AL                 |
+| SOOPSTENIJA_AL                 |
+| SOOPSTGRAFIKONI                |
+| SOOPSTTABELI                   |
+| STATUSSOOP_AL                  |
+| STATUSSOOP_AL                  |
+| TERMINOLOSKIOBJASNUVANJA       |
+| TIPNAZBIRKI                    |
+| Tabeli                         |
+| Table_1                        |
+| Table_2                        |
+| ZANASGLAVNA_AL                 |
+| ZANASGLAVNA_AL                 |
+| ZANAS_AL                       |
+| ZANAS_AL                       |
+| ZANIMLIVOSTI                   |
+| meta_vrski                     |
+| sysdiagrams                    |
++--------------------------------+
+
+Database: msdb
+[9 tables]
++--------------------------------+
+| backupfile                     |
+| backupmediafamily              |
+| backupmediaset                 |
+| backupset                      |
+| logmarkhistory                 |
+| restorefilegroup               |
+| restorefilegroup               |
+| restorehistory                 |
+| suspect_pages                  |
++--------------------------------+
+
+Database: WEBtestInj
+[55 tables]
++--------------------------------+
+| AKTIVNOSTI                     |
+| ARHIVA_PUBL                    |
+| ARHIVA_PUBL                    |
+| DOWNLOADS                      |
+| DOWNLOADSXLS                   |
+| EDICIIPUBLIKACII               |
+| FORMATPUBLIKUVANJE             |
+| GLAVNIOBLASTI                  |
+| GODINI                         |
+| GRAFIK                         |
+| INDIKATORIGLAVNA               |
+| INDIKATORI_TS                  |
+| INDIKATORI_TS                  |
+| INSTITUCII                     |
+| IZVESTAJKVALITETGLAVNA         |
+| IZVESTAJKVALITETGLAVNA         |
+| JAZIK                          |
+| KALENDAR_MESECI                |
+| KALENDAR_MESECI                |
+| KALENDAR_NOV_VNES              |
+| KALKULATOR_COICOP              |
+| KLASIFIKACIIGLAVNA             |
+| KLASIFIKACIIGLAVNA             |
+| LEGISLATIVA                    |
+| MAPI_PY                        |
+| META                           |
+| METODOLOGII                    |
+| METODOLOSKIOBJASNUVANJA_ARHIVA |
+| METODOLOSKIOBJASNUVANJA_ARHIVA |
+| OBLASTEKSTRA                   |
+| OBLASTI_MAKSTAT                |
+| OBLASTI_MAKSTAT                |
+| OBLASTOPSTO                    |
+| POPISNI_REONI                  |
+| POPIS_MEDIUMI_EN               |
+| POPIS_MEDIUMI_MK               |
+| PRIBIRANJEPODATOCI             |
+| PUBLIKACIIGLAVNA               |
+| PUBLIKACIIGLAVNA               |
+| RSS                            |
+| SEDUMDESET                     |
+| SOOPSTENIJA                    |
+| SOOPSTENIJATEKST               |
+| SOOPSTGRAFIKONI                |
+| SOOPSTTABELI                   |
+| STATUSSOOP                     |
+| TERMINOLOSKIOBJASNUVANJA       |
+| TIPNAZBIRKI                    |
+| Tabeli                         |
+| Table_1                        |
+| Table_2                        |
+| ZANASGLAVNA                    |
+| ZANASGLAVNA                    |
+| ZANIMLIVOSTI                   |
+| sysdiagrams                    |
++--------------------------------+
