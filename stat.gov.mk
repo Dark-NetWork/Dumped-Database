f67d8569044ce234ea952157f7836de8f115a28c
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-24 10:25:18 +0300

    Update stat.gov.mk

--- stat.gov.mk
+++ stat.gov.mk
@@ -10,6 +10,11 @@ State Institute of Statistics
 
 http://www.stat.gov.mk/OblastOpsto_en.aspx?id=13
 
+Microsoft SQL Server 2008 (SP3) - 10.0.5512.0 (X64) 
+        Aug 22 2012 19:25:47 
+        Copyright (c) 1988-2008 Microsoft Corporation
+        Enterprise Edition (64-bit) on Windows NT 6.0 <X64> (Build 6003: Service Pack 2) (VM)
+
 the back-end DBMS is Microsoft SQL Server
 web server operating system: Windows 2012 R2 or 8.1
 web application technology: ASP.NET 4.0.30319, ASP.NET, Microsoft IIS 8.5
