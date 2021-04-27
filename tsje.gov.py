b17c2b826ec9f78823de1df761e989f4400ab66e
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-27 14:20:57 +0300

    Create tsje.gov.py

---
+++ tsje.gov.py
@@ -0,0 +1,178 @@
+.gov Government
+
+.py Paraguay
+
+Electoral Justice · Republic of Paraguay
+
+Custodian of the Popular Will
+
+https://tsje.gov.py/sede/form_ver_sede.php?id=161&ver=si
+
+the back-end DBMS is PostgreSQL
+web server operating system: Linux CentOS 7
+web application technology: PHP 5.6.30, Apache 2.4.6
+the back-end DBMS: PostgreSQL
+banner: 'PostgreSQL 9.2.2 on x86_64-unknown-linux-gnu, compiled by gcc (GCC) 4.1.2 20080704 (Red Hat 4.1.2-52), 64-bit'
+
+available databases [1]:
+[+] public
+
+Database: public
+[107 tables]
++-------------------------------------+
+| fppml_solicitudes-4ta-mayo          |
+| fppml_solicitudes_08_05-2019        |
+| legislaciones-prueba                |
+| solicitudes_hasta _27_03_18         |
+| actividades_ssee                    |
+| audios                              |
+| banners                             |
+| bloques                             |
+| calendario                          |
+| calendario_copy                     |
+| calendario_viejo                    |
+| categoria_calendario                |
+| columnas                            |
+| conferencia_envios                  |
+| conferencia_inscripcion             |
+| conferencia_inscripcion2            |
+| conferencia_inscripcion_copy        |
+| conferencia_inscripcion_copy1       |
+| conferencia_inscripcion_copy_copy   |
+| conferencia_inscripcion_dom         |
+| conferencia_inscripcion_lun06       |
+| conferencia_inscripcion_sab         |
+| conferencia_inscripcion_sab2        |
+| conferencia_lista_comple_inscriptos |
+| credenciales                        |
+| credenciales_areas                  |
+| credenciales_modulos                |
+| credenciales_permisos               |
+| credenciales_tipos                  |
+| credenciales_usuarios               |
+| departamento                        |
+| departamentos                       |
+| departamentos_sedes                 |
+| departamentos_v                     |
+| distritos                           |
+| distritos_sedes                     |
+| distritos_v                         |
+| documentos                          |
+| enlaces                             |
+| faqs                                |
+| faqs_temas                          |
+| form_proponentes                    |
+| fppml_modulos                       |
+| fppml_permisos                      |
+| fppml_solicitudes                   |
+| fppml_solicitudes_2018              |
+| fppml_solicitudes_3ra_edicion       |
+| fppml_solicitudes_4ta_edicion       |
+| fppml_solicitudes_7_edicion         |
+| fppml_solicitudes_old               |
+| fppml_usuarios                      |
+| galerias                            |
+| galerias_guarderia                  |
+| galerias_mecip                      |
+| galerias_mujeres                    |
+| galerias_ppm                        |
+| galerias_ssee                       |
+| ge_locales_registros                |
+| ge_nexos                            |
+| ge_personales                       |
+| ge_registros                        |
+| habilitado                          |
+| habilitado_copy                     |
+| idiomas                             |
+| legislaciones                       |
+| legislaciones_tipos                 |
+| legislaciones_tipos_junio           |
+| libros                              |
+| loc                                 |
+| locales_registros                   |
+| locales_registros_copy              |
+| locales_registros_copy1             |
+| locales_registros_new               |
+| locales_registros_old_2018          |
+| mensajes                            |
+| menu                                |
+| modulos                             |
+| noticias                            |
+| padron_abg_definitivo               |
+| padron_sitrande                     |
+| padrones                            |
+| paginas                             |
+| partidos_politicos                  |
+| partidos_politicos_tipos            |
+| partidos_proponentes                |
+| permisos                            |
+| proponentes                         |
+| proponentes_viejo_con_errores       |
+| redactores                          |
+| roles                               |
+| settings                            |
+| solicitudes                         |
+| sys_auditoria                       |
+| sys_dependencias                    |
+| sys_login                           |
+| sys_modulos                         |
+| sys_permisos                        |
+| sys_secciones                       |
+| sys_usuarios                        |
+| taller_solicitudes                  |
+| usuarios                            |
+| usuarios_log                        |
+| usuarios_reg                        |
+| usuarios_sysva                      |
+| zona                                |
+| zonas                               |
+| zonas_sedes                         |
++-------------------------------------+
+
+Database: public
+Table: fppml_usuarios
+[8 entries]
++----------------+--------------+--------------+
+| usuario_nombre | usuario_user | usuario_pass |
++----------------+--------------+--------------+
+| Admin          | admin        | 147258369    |
+| jorge          | jorge        | jorge963     |
+| CYNTHIA        | cynthia      | cynthia      |
+| MARCELO        | cmamarilla   | cmamarilla   |
+| FEDERICO       | federico     | federico123  |
+| Cesar          | cesarr       | cesarr       |
+| g              | gsanabria    | gsanabria    |
+| ntd            | ntd          | ntd          |
++----------------+--------------+--------------+
+
+Database: public                                                                                                                                                     
+Table: credenciales_usuarios
+[7 entries]
++----------------+--------------+------------------------------------------+
+| usuario_nombre | usuario_user | usuario_pass                             |
++----------------+--------------+------------------------------------------+
+| alejandra      | ale          | 12345                                    |
+| amilcar        | amilcar      | amilcar123                               |
+| raul           | raguero      | raguero                                  |
+| irina          | irina        | irina                                    |
+| ALEJANDRA      | aleboga      | 827ccb0eea8a706c4c34a16891f84e7b (12345) |
+| ALEJANDRA      | aleboga      | 827ccb0eea8a706c4c34a16891f84e7b (12345) |
+| JORGE          | jorge        | 202cb962ac59075b964b07152d234b70 (123)   |
++----------------+--------------+------------------------------------------+
+
+Database: public
+Table: usuarios
+[9 entries]
++--------------------------+------------------+-----------------------------+------------------------------------------+
+| nombre                   | usuario          | email                       | password                                 |
++--------------------------+------------------+-----------------------------+------------------------------------------+
+| Maria Oviedo             | maria-oviedo     | maguille36@gmial.com        | a93e5c75323a44476c491c62a8ab111916bd423e |
+| Leticia Estigarribia     | leticia          | lestigarribia@tsje.gov.py   | fc1c1977090a525c702f7f54cea06290f417fd11 |
+| Katerin Saldivar         | katerin-saldivar | Katherinsaldivar1@gmail.com | 805708da65447a225272b2a2bd2c987610be2b5f |
+| Juan Paez                | juanma           | jpaez@tsje.gov.py           | 25d58f195edafb3141d1c88c0c8732f0b53653ed |
+| Juan Luis Gonzalez       | juan-luis        | juangfrutos@gmail.com       | 657c0af9b5eea95123b7258637aeb25e8ff976bd |
+| Yahinie Marysel Martinez | marysel          | ymartinez@tsje.gov.py       | 121c4f64bfcac153823936d69f1aa475a2e3974a |
+| Rose Marie Barrios       | rose-marie       | prensa@tsje.gov.py          | 05bbc28dcfba1546f342421fb802677dffc16e87 |
+| Irina Goiburu            | irina-goiburu    | irina@tsje.gov.py           | 9eb76d0b6a4567bf83400e2e7827aa21e81fa00f |
+| Juan Jose Romañach       | juancho          | jromanach_py@hotmail.com    | 8394f3082af819e7d6ce2c305e2d23191a1bb1c6 |
++--------------------------+------------------+-----------------------------+------------------------------------------+
